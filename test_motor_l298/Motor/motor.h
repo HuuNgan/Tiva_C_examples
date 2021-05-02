1ad6e4961090b1d6e7ec0b4aeb65c2f2b2b4dad9
HuuNgan <ngan.luongbkdev@hcmut.edu.vn>
2021-05-02 12:08:28 +0700

    add test motor l298

---
+++ test_motor_l298/Motor/motor.h
@@ -0,0 +1,55 @@
+/*
+ * motor.h
+ *
+ *  Created on: May 7, 2019
+ *      Author: HuuNgan
+ */
+
+#ifndef MOTOR_MOTOR_H_
+#define MOTOR_MOTOR_H_
+
+
+//*****************************************************************************
+//This program using these Pins:
+//PC4, PC5, PC6, PC7, PD0, PD1
+//If you want to change PORT, remember to check PWM_GEN.
+
+#define MOTOR_PORT       GPIO_PORTC_BASE
+#define motorA_Pin1      GPIO_PIN_4
+#define motorA_Pin2      GPIO_PIN_5
+#define motorB_Pin1      GPIO_PIN_6
+#define motorB_Pin2      GPIO_PIN_7
+#define MOTOR_PERIPH     SYSCTL_PERIPH_GPIOC
+
+#define PWM_PORT         GPIO_PORTD_BASE
+#define PWM_Pin_A        GPIO_PIN_0
+#define PWM_Pin_B        GPIO_PIN_1
+#define PWM_PERIPH       SYSCTL_PERIPH_GPIOD
+#define PWM_A_Config     GPIO_PD0_M1PWM0
+#define PWM_B_Config     GPIO_PD1_M1PWM1
+#define PWM_OUT_A        PWM_OUT_0
+#define PWM_OUT_B        PWM_OUT_1
+#define PWM_OUT_A_BIT    PWM_OUT_0_BIT
+#define PWM_OUT_B_BIT    PWM_OUT_1_BIT
+
+#define PWM_DUTY_MAX     1000
+#define PWM_FREQ         40000
+#define MAX_PULSE_WIDTH  (SysCtlClockGet()/PWM_FREQ-1)
+//SysCtlPWMClockGet() return the value of the divider which locating in 16 high significant bits
+
+#define backward  'b'
+#define forward  'f'
+
+//*****************************************************************************
+void motorA_run(char direction);
+void motorB_run(char direction);
+void release_A(void);
+void release_B(void);
+void setSpeed_A(int speed);
+void setSpeed_B(int speed);
+
+void PWM_Config(void);
+void MOTOR_Config(void);
+
+
+#endif /* MOTOR_MOTOR_H_ */
---
+++ test_motor_l298/main.c
@@ -0,0 +1,208 @@
+//*****************************************************************************
+//----------------------------DC MOTOR CONTROL---------------------------------
+//--------------------------Pay It Forward Club--------------------------------
+//-------------------------Author: LUONG HUU NGAN------------------------------
+//-------------------------Created on: May 7, 2019-----------------------------
+//-------------------------Feel Free to copy it--------------------------------
+//*****************************************************************************
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/debug.h"
+#include "driverlib/pwm.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/systick.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/timer.h"
+#include "inc/hw_gpio.h"
+#include "Motor/motor.h"
+#include "QEI/QEI.h"
+
+enum {Release, RunA, ENCODER, PID, ENC_Position, PID_Position} Mode;
+typedef struct
+{
+    volatile int speed, direction;
+    uint16_t position, stopPosition;
+}Encoder;
+
+typedef struct
+{
+    float Kp, Ki, Kd, speed, current_err, previous_err;
+    float iteration_sum, iteration_time;
+    uint16_t positionSet;
+}PID_coefficient;
+
+float dutyA = 500, dutyB=500;
+uint32_t sysTickValue;
+uint8_t sysTickCount;
+uint8_t timerCount;
+Encoder encoderA, encoderB;
+PID_coefficient PID_B={1.3, 0.000035, 0.0005, 50};
+
+//*****************************************************************************
+//
+// The error routine that is called if the driver library encounters an error.
+//
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+    while(1);
+}
+#endif
+
+//*****************************************************************************
+void timerInit(void)
+{
+//    #define INT_TIMER0A     0x23
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+    TimerLoadSet(TIMER0_BASE, TIMER_A, SysCtlClockGet()-1);
+
+//    IntEnable(INT_TIMER0A);
+//    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+//    IntMasterEnable();
+
+    TimerEnable(TIMER0_BASE, TIMER_A);
+}
+
+void SysTickConfig(void)
+{
+    SysTickPeriodSet(SysCtlClockGet()/10);
+
+    // Enable interrupts to the processor.
+    IntMasterEnable();
+    // Enable the SysTick Interrupt.
+    SysTickIntEnable();
+    // Enable SysTick.
+    SysTickEnable();
+}
+
+float getTimeValue(void)
+{
+    uint32_t time_duration, timerValue;
+    static int previousTimerValue=0;
+    timerValue = TimerValueGet(TIMER0_BASE, TIMER_A);
+    if(timerValue >= previousTimerValue)
+    {
+        time_duration = timerValue - previousTimerValue;
+    }
+    else
+    {
+        time_duration = SysCtlClockGet() - previousTimerValue + timerValue;
+    }
+    sysTickValue = time_duration;
+    previousTimerValue = timerValue;
+    return time_duration*0.1/(SysCtlClockGet()/10);
+}
+
+void SysTickIntHandler(void)
+{
+    sysTickCount ++;
+}
+
+void Timer0IntHandler(void)
+{
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    timerCount++;
+}
+
+int main(void)
+{
+    //Initialize clock
+    SysCtlClockSet(SYSCTL_SYSDIV_2_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+    PWM_Config();
+    MOTOR_Config();
+    Config_Qei();
+//    SysTickConfig();
+    timerInit();
+
+    Mode = PID_Position;
+    PID_B.iteration_time = SysTickValueGet();
+    encoderB.stopPosition = 1495;
+
+    //Loop forever
+    while(1)
+    {
+        switch(Mode)
+        {
+            case RunA:
+                motorA_run(forward);
+                for(dutyA=400; dutyA<=1000; dutyA++)
+                {
+                    setSpeed_A(dutyA);
+                    encoderA.speed = ROM_QEIVelocityGet(QEI0_BASE);
+                    encoderA.direction = ROM_QEIDirectionGet(QEI0_BASE);
+                    SysCtlDelay(100000);
+                }
+                setSpeed_A(1);
+                SysCtlDelay(50000000);
+
+                motorA_run(backward);
+                for(dutyA=400; dutyA<=1000; dutyA++)
+                {
+                    setSpeed_A(dutyA);
+                    encoderA.speed = ROM_QEIVelocityGet(QEI0_BASE);
+                    encoderA.direction = ROM_QEIDirectionGet(QEI0_BASE);
+                    SysCtlDelay(100000);
+                }
+                setSpeed_A(1);
+                SysCtlDelay(50000000);
+                break;
+            case ENCODER:
+                setSpeed_B(dutyB);
+                motorB_run(forward);
+                encoderB.speed = ROM_QEIVelocityGet(QEI0_BASE)*60/1496;
+                encoderB.direction = ROM_QEIDirectionGet(QEI0_BASE);
+                break;
+            case PID:
+                PID_B.iteration_time = getTimeValue();
+                encoderB.direction = ROM_QEIDirectionGet(QEI0_BASE);
+                encoderB.speed = ROM_QEIVelocityGet(QEI0_BASE)*60/1496;
+                PID_B.current_err = PID_B.speed - encoderB.speed;
+                PID_B.iteration_sum += PID_B.current_err*PID_B.iteration_time;
+                dutyB = PID_B.Kp*PID_B.current_err + PID_B.Ki*PID_B.iteration_sum
+                        + PID_B.Kd*(PID_B.current_err-PID_B.previous_err)/PID_B.iteration_time;
+                if(dutyB<0) dutyB = 1;
+                setSpeed_B(dutyB);
+                motorB_run(forward);
+                PID_B.previous_err = PID_B.current_err;
+                break;
+            case ENC_Position:
+                setSpeed_B(500);
+                motorB_run(forward);
+                encoderB.position = ROM_QEIPositionGet(QEI0_BASE);
+                while(encoderB.position < encoderB.stopPosition)
+                {
+                    motorB_run(forward);
+                    encoderB.position = ROM_QEIPositionGet(QEI0_BASE);
+                }
+                release_B();
+                SysCtlDelay(SysCtlClockGet()/3);
+                break;
+            case PID_Position:
+                PID_B.iteration_time = getTimeValue();
+                encoderB.position = ROM_QEIPositionGet(QEI0_BASE);
+                if(encoderB.position > PID_B.positionSet)    motorB_run(backward);
+                else if(encoderB.position < PID_B.positionSet)    motorB_run(forward);
+                PID_B.current_err = PID_B.positionSet - encoderB.position;
+                PID_B.iteration_sum += PID_B.current_err*PID_B.iteration_time;
+                dutyB = PID_B.Kp*PID_B.current_err + PID_B.Ki*PID_B.iteration_sum
+                        + PID_B.Kd*(PID_B.current_err-PID_B.previous_err)/PID_B.iteration_time;
+                if(dutyB >= 0)  setSpeed_B(dutyB);
+                else    setSpeed_B(-dutyB);
+                PID_B.previous_err = PID_B.current_err;
+                break;
+            case Release:
+                release_A();
+                release_B();
+                break;
+        }
+    }
+}
