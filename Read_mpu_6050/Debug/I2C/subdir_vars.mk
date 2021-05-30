72dc0142bc19d581297469fe0438de316c95c7c4
HuuNgan <ngan.luongbkdev@hcmut.edu.vn>
2021-05-30 23:51:21 +0700

    edit include path

---
+++ Read_mpu_6050/Debug/I2C/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../I2C/mpu6050.c 
+
+C_DEPS += \
+./I2C/mpu6050.d 
+
+OBJS += \
+./I2C/mpu6050.obj 
+
+OBJS__QUOTED += \
+"I2C\mpu6050.obj" 
+
+C_DEPS__QUOTED += \
+"I2C\mpu6050.d" 
+
+C_SRCS__QUOTED += \
+"../I2C/mpu6050.c" 
+
+
